2249c9c6447920a315651e0062425fdbefdfd15c
Danylo-Rysak <rysak.d.2002@gmail.com>
2023-10-14 18:33:32 +0300

    DR-added-fonts-and-icons

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // Components
 import App from './App.tsx';
+// Fonts
+import 'assets/fonts/fonts.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
